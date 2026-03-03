a62512ba0ccc9915815f6cd78f67265fb15c0290
Rasmus <87243876+The1lama@users.noreply.github.com>
2026-03-03 11:15:07 +0100

    Working snake,
    Have enum for gridValue

--- SnakeConsole/SnakeConsole.cpp
+++ SnakeConsole/SnakeConsole.cpp
@@ -12,7 +12,7 @@
 #pragma region Defines
 
 #define CLEAR_SCREEN std::cout << "\x1b[2J\x1b[H";
-#define SizeToInt(x)  static_cast<int>(x)
+#define ToInt(x)  static_cast<int>(x)
 #define twoDArray std::vector<std::vector<int>>
 
 struct Vector2Int
@@ -46,8 +46,8 @@ void SpawnMainMenu();
 
 #pragma endregion Functions
 
-#pragma region Helpers
-
+namespace Helper
+{
 int static GetRandomNumber(int max)
 {
     // Obtains a random number from hardware to seed 
@@ -60,7 +60,7 @@ int static GetRandomNumber(int max)
     return dist(gen);
 }
 
-void hideCursor(const bool show)
+void ShowCursor(const bool show)
 {
     HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO cursor;
@@ -69,56 +69,52 @@ void hideCursor(const bool show)
     SetConsoleCursorInfo(consoleHandle, &cursor);
 }
 
-void cls()
+void ClearScreen()
 {
-    static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), COORD{0,0});
+}
     
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    COORD topLeft {0,0};
-    
-    std::cout.flush();
-    
-    if (!GetConsoleScreenBufferInfo(hOut, &csbi))
-    {
-        // TODO: Handle failure!
-        system("cls");
-        return;
-    }
-    DWORD lenght = csbi.dwSize.X * csbi.dwSize.Y;
-    DWORD written;
-    
-    FillConsoleOutputCharacter(hOut, TEXT(' '), lenght, topLeft, &written);
-    
-    FillConsoleOutputAttribute(hOut, csbi.wAttributes, lenght, topLeft, &written);
-    
-    SetConsoleCursorPosition(hOut, topLeft);
 }
 
-#pragma endregion Helpers
+class GridCell
+{
+public:
+    int x;
+    int y;
+    char characterChar;
+};
+
+enum class GridValue
+{
+    Empty,
+    Wall,
+    Food,
+    Player,
+};
 
 #pragma region Grid
 
 void RenderGrid(twoDArray& grid_area)
 {
     //CLEAR_SCREEN
-    cls();
-    for (int y = 0; y < SizeToInt(grid_area[0].size()); y++)
+    Helper::ClearScreen();
+    for (int y = 0; y < ToInt(grid_area[0].size()); y++)
     {
-        for (int x = 0; x < SizeToInt(grid_area.size()); x++)
+        for (int x = 0; x < ToInt(grid_area.size()); x++)
         {
             switch (grid_area[x][y])
             {
-            case 1:     // is wall
+            case ToInt(GridValue::Wall):     // is wall
                 std::cout << '#';
                 break;
-            case 2:     // is food
+            case ToInt(GridValue::Food):     // is food
                 std::cout << '*';
                 break;
-            case 3:
+            case ToInt(GridValue::Player):     // Player
                 std::cout << '@';
                 break;
             default:    // is white space
-                std::cout << ' ';
+                std::cout << " ";
             }
         }
         std::cout << '\n';
@@ -127,12 +123,24 @@ void RenderGrid(twoDArray& grid_area)
 
 bool IsInsideGrid(int x, int y, const twoDArray& gridArea)
 {
-    return x > 0 && x < SizeToInt(gridArea.size()-1)
-        && y > 0 && y < SizeToInt(gridArea[0].size()-1);
+    return x > 0 && x < ToInt(gridArea.size()-1)
+        && y > 0 && y < ToInt(gridArea[0].size()-1);
 }
-int CheckGridValue(int x, int y, const twoDArray& gridArea)
+GridValue GetGridValue(int x, int y, const twoDArray& gridArea)
 {
-    return gridArea[x][y];
+    switch (gridArea[x][y])
+    {
+        case ToInt(GridValue::Wall):
+            return GridValue::Wall;
+        case ToInt(GridValue::Food):
+            return GridValue::Food;
+        case ToInt(GridValue::Player):
+            return GridValue::Player;
+        case ToInt(GridValue::Empty):
+        default: ;
+            return GridValue::Empty;
+    }
+    
 }
 
 twoDArray GenerateGrid(int width, int height)
@@ -162,8 +170,8 @@ twoDArray GenerateGrid(int width, int height)
 void SpawnFood(twoDArray& grid_area, std::vector<Vector2Int> snakeBody)
 {
     // Gets a random pos in the grid_area
-    int randX { GetRandomNumber(SizeToInt(grid_area.size())) - 1  };
-    int randY { GetRandomNumber(SizeToInt(grid_area[0].size())) - 1 };
+    int randX {Helper::GetRandomNumber(ToInt(grid_area.size())) - 1  };
+    int randY {Helper::GetRandomNumber(ToInt(grid_area[0].size())) - 1 };
     
     // checks if the random pos is inside the grid_area
     // if not rerun this function until it has found a valid pos
@@ -173,8 +181,9 @@ void SpawnFood(twoDArray& grid_area, std::vector<Vector2Int> snakeBody)
         SpawnFood(grid_area, snakeBody);
         return;
     }
-    int vectorValue { CheckGridValue(randX, randY, grid_area) };
-    if (vectorValue == 1 || vectorValue == 3)
+    // if the food spawns inside a wall or player it finds a new random place to spawn
+    GridValue vectorValue { GetGridValue(randX, randY, grid_area) };
+    if (vectorValue == GridValue::Wall || vectorValue == GridValue::Player)
     {
         SpawnFood(grid_area, snakeBody);
         return;
@@ -182,9 +191,95 @@ void SpawnFood(twoDArray& grid_area, std::vector<Vector2Int> snakeBody)
     grid_area[randX][randY] = 2;
 }
 
+// Gets player input when playing snake
+void GetPlayerInput(bool& running, Vector2Int directionValue, Vector2Int& temporaryDirectionValue)
+{
+    if ( _kbhit() )
+    {
+        switch (_getch())
+        {
+        case 97:    // [A] west
+            if (directionValue != Vector2Int{1,0}) // if the direction is not the opposite side of the moving direction
+                temporaryDirectionValue = {-1,0};
+            break;
+        case 119:   // [W] north
+            if (directionValue != Vector2Int{0,-1}) // if the direction is not the opposite side of the moving direction
+                temporaryDirectionValue = {0,-1};
+            break;
+        case 100:   // [D] east
+            if (directionValue != Vector2Int{-1,0}) // if the direction is not the opposite side of the moving direction
+                temporaryDirectionValue= {1,0};
+            break;
+        case 115:   // [S] south
+            if (directionValue != Vector2Int{0,-1}) // if the direction is not the opposite side of the moving direction
+                temporaryDirectionValue = {0,1};
+            break;
+        case 113:   // [Q] To main menu
+            running = false;
+            break;
+        default:
+            break;
+        }   
+    }
+}
+
+// Updates the snake position on grid 
+void UpdateSnakePosition(bool& running, std::vector<Vector2Int>& snakeBody, std::vector<std::vector<int>>& gridArea, Vector2Int temporaryDirectionValue)
+{
+    // for initializing the snake start body position, Start from snake head
+    Vector2Int lastBodyPosition = snakeBody[0];
+    bool SnakeGrowing { false };
+    // for moving the whole snake
+    for (int i = 0; i < ToInt(snakeBody.size()); ++i)
+    {
+        Vector2Int snakeBodyPart { snakeBody[i] };
+        Vector2Int newSnakePosition;
+        // check if snake part is body or head
+        if ( i != 0 )
+        {
+            // move by lastBodyPosition
+            newSnakePosition = { lastBodyPosition.x, lastBodyPosition.y };
+        }
+        else
+        {
+            // Move the snake head by direction
+            newSnakePosition = { snakeBody[0].x + temporaryDirectionValue.x, snakeBody[0].y + temporaryDirectionValue.y };
+                    
+            // Check the next position the snake is going to hit,
+            //if the snake hits wall(1) or a body part of the snake(3), then break the game loop
+            GridValue nextGridValue { GetGridValue(newSnakePosition.x, newSnakePosition.y, gridArea) };
+            if (nextGridValue == GridValue::Wall || (nextGridValue == GridValue::Player))
+            {
+                running = false;
+                break;
+            }
+            //else if snake hits food(2), make the snake longer
+            else if (nextGridValue == GridValue::Food)
+                SnakeGrowing = true;
+                    
+        }
+        // updates snake body part position
+        snakeBody[i] = newSnakePosition;
+        // Removes old snake part (for Drawing)
+        gridArea[snakeBodyPart.x][snakeBodyPart.y] = ToInt(GridValue::Empty);
+        // Draws new snake part at new position 
+        gridArea[newSnakePosition.x][newSnakePosition.y] = ToInt(GridValue::Player);
+        // sets the current body position so the next position can read where it should go
+        lastBodyPosition = snakeBodyPart;
+    }
+    if (SnakeGrowing)
+    {
+        // creates a new snake body part and 
+        // spawn in a new food at random position
+        snakeBody.push_back(lastBodyPosition);
+        SpawnFood(gridArea, snakeBody);
+    }
+}
+
 void PlaySnake()
 {
-    hideCursor(FALSE);
+    CLEAR_SCREEN        // removes menu screen text 
+    Helper::ShowCursor(FALSE);      // Hides cursor
     bool running { true };
     
     // width and height of the grid
@@ -192,15 +287,9 @@ void PlaySnake()
     int height { 10 };
     
     // snake movement
-    Vector2Int direction { 1,0 };
-    // only for snake head
-    Vector2Int snakeHeadPosition {width/2,height/2};
-    //   Vector2Int snakeBody1 {6,5};
-    //   Vector2Int snakeBody2 {6,4};
-    //   Vector2Int snakeBody3 {6,3};
-    //  Vector2Int snakeBody4 {6,2};
-    //   Vctor2Int snakeBody5 {6,1};
-    std::vector<Vector2Int> snakeBody {snakeHeadPosition};
+    Vector2Int directionValue { 1,0 };
+    // creates the snake body and initializes it with it's head at the grid middle
+    std::vector<Vector2Int> snakeBody {Vector2Int{width/2,height/2}};
     
     twoDArray gridArea {GenerateGrid(width,height) };
     SpawnFood(gridArea, snakeBody);
@@ -208,93 +297,23 @@ void PlaySnake()
     
     // set timer for how often the snake updates
     auto lastUpdate = std::chrono::steady_clock::now();
-    std::chrono::milliseconds interval {250};
+    std::chrono::milliseconds interval {200};
+    Vector2Int temporaryDirectionValue = directionValue;
     
-    
+    // Snake game loop
     while (running)
     {
         auto now = std::chrono::steady_clock::now();
-        if ( _kbhit() )
-        {
-            switch (_getch())
-            {
-            case 97:    // [A] west
-                if (direction != Vector2Int{1,0}) // if the direction is not the opposite side of the moving direction
-                    direction = {-1,0};
-                break;
-            case 119:   // [W] north
-                if (direction != Vector2Int{0,-1}) // if the direction is not the opposite side of the moving direction
-                    direction = {0,-1};
-                break;
-            case 100:   // [D] east
-                if (direction != Vector2Int{-1,0}) // if the direction is not the opposite side of the moving direction
-                    direction = {1,0};
-                break;
-            case 115:   // [S] south
-                if (direction != Vector2Int{0,-1}) // if the direction is not the opposite side of the moving direction
-                    direction = {0,1};
-                break;
-            case 113:   // [Q] To main menu
-                running = false;
-                break;
-            default:
-                break;
-            }   
-        }
+        // Gets the player input and stores the value inside temporaryDirectionValue
+        GetPlayerInput(running, directionValue, temporaryDirectionValue);
         
         if (now - lastUpdate >= interval)
         {
-            // for initializing the snake start body position, Start from snake head
-            Vector2Int lastBodyPosition {snakeHeadPosition};
-            bool SnakeGrowing { false };
-            // for moving the whole snake
-            for (int i = 0; i < SizeToInt(snakeBody.size()); ++i)
-            {
-                Vector2Int snakeBodyPart { snakeBody[i] };
-                Vector2Int newSnakePosition;
-                // check if snake part is body or head
-                if ( i != 0 )
-                {
-                    // move by lastBodyPosition
-                    newSnakePosition = { lastBodyPosition.x, lastBodyPosition.y };
-                }
-                else
-                {
-                    // Move the snake head by direction
-                    newSnakePosition = { snakeHeadPosition.x + direction.x, snakeHeadPosition.y + direction.y };
-                    
-                    // Check the next position the snake is going to hit,
-                    //if the snake hits wall(1) or a body part of the snake(3), then break the game loop
-                    int nextGridValue { CheckGridValue(newSnakePosition.x, newSnakePosition.y, gridArea) };
-                    if (nextGridValue == 1 || (nextGridValue == 3))
-                    {
-                        running = false;
-                        break;
-                    }
-                    //else if snake hits food(2), make the snake longer
-                    else if (nextGridValue == 2)
-                        SnakeGrowing = true;
-                    
-                    snakeHeadPosition = newSnakePosition;
-                }
-                // updates snake body part position
-                snakeBody[i] = newSnakePosition;
-                // Removes old snake part (for Drawing)
-                gridArea[snakeBodyPart.x][snakeBodyPart.y] = 0;
-                // Draws new snake part at new position 
-                gridArea[newSnakePosition.x][newSnakePosition.y] = 3;
-                // sets the current body position so the next position can read where it should go
-                lastBodyPosition = snakeBodyPart;
-            }
-            if (SnakeGrowing)
-            {
-                // creates a new snake body part and 
-                // spawn in a new food at random position
-                snakeBody.push_back(lastBodyPosition);
-                SpawnFood(gridArea, snakeBody);
-            }
+            UpdateSnakePosition(running, snakeBody, gridArea, temporaryDirectionValue);
             lastUpdate = now;
             RenderGrid(gridArea);
+            if (directionValue != temporaryDirectionValue)  // Sets the temporaryDirectionValue to the direction value, if it has changed
+                directionValue = temporaryDirectionValue;   
         }
     }
     
@@ -303,7 +322,6 @@ void PlaySnake()
     std::cout << "Ending Snake game";
     while (!_kbhit()){}
     SpawnMainMenu();
-    CLEAR_SCREEN
 }
 
 void DrawHelpMenu()
@@ -320,7 +338,7 @@ void DrawHelpMenu()
 
 void SpawnMainMenu()
 {
-    hideCursor(TRUE);
+    Helper::ShowCursor(TRUE);
     bool running { true };
     
     while (running)
@@ -329,7 +347,7 @@ void SpawnMainMenu()
         std::cout << "===== Welcome to Snake =====\n";
         std::cout << "Press a number to choose what to do\n";
         std::cout << "1) Play Snake \n" << "2) Controls/Help\n" << "3) Quit\n";
-        std::cout << "> ";
+        std::cout << ">>> ";
         
         int choice;
         std::cin >> choice;
